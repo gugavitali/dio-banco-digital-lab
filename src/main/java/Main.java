1422b2210deec7726af6dadafca6efc0c1d06ee4
gugavitali <gugavitali2004@gmail.com>
2024-12-23 23:06:43 -0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import model.comuns.Cliente;
+import model.comuns.ContaCorrente;
+import model.comuns.ContaPoupanca;
+import model.abstratas.Conta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
+
+        Conta cc = new ContaCorrente(venilton);
+        Conta poupanca = new ContaPoupanca(venilton);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+
+}
---
+++ src/main/java/model/comuns/ContaCorrente.java
@@ -0,0 +1,18 @@
+package model.comuns;
+
+import model.abstratas.Conta;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+
+}
+
